a3044b7146444af60873b51fea4d0ba21dfd10fc
JmDictMod <mdsahajan2016new@gmail.com>
2025-03-04 21:24:48 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar";
 import SearchResults from "./components/ResultsList";
 import "./App.css";
